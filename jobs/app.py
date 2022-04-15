66289c310adc46d83c3777acd9c6dedfa570f0aa
irmk323 <irmk323@yahoo.co.jp>
2022-04-15 18:17:56 +0100

    added connection

--- jobs/app.py
+++ jobs/app.py
@@ -6,8 +6,28 @@ app = Flask(__name__)
 
 
 def open_connection():
-  connection =  getattr('_connection')
+  connection =  getattr(g, '_connection' , None)
+  if connection == None:
+      connection = g._connection = sqlite3.connect(PATH)
+  connection.row_factory = sqlite3.Row
   return connection
+
+def execute_sql(sql, values=(),commit=False, single=False):
+    connection = open_connection()
+    cursor = connection.execute(sql, values)
+    if commit == True:
+       results = connection.commit()
+    else:
+       results = cursor.fetchone() if single else cursor.fetchall()
+    cursor.close()
+    return results
+
+# 後始末メソッド　app.teardown_appcontext
+@app.teardown_appcontext
+def close_connection(exception):
+    connection =  getattr(g, '_connection' , None)
+    if connection is not None:
+       connection.close()
 
 @app.route('/')
 @app.route("/jobs")
